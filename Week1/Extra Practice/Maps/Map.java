40392b6fa7cc851443752848ab8879b14b0ff0c3
DeWittDev <88995967+DeWittDev@users.noreply.github.com>
2022-02-12 22:23:10 -0800

    Copybook 3

--- Week1/Extra Practice/Maps/Map.java
+++ Week1/Extra Practice/Maps/Map.java
@@ -10,6 +10,7 @@ public class Map {
         idMap.put("Copybook", "the burnt fool's bandaged finger went wobbling back to the fire");
         idMap.put("Headings", "Thus when it happened and the brave new world began, when all men where payed for his suffering, and no man payed for his sins. As surely as water has wet us, as surely as fire has burned. the Gods of the Copybook Headings with terror and slaughter, return.");
 
+        System.out.println(idMap.get("Headings") + "\n\n");
         Set<String> keys = idMap.keySet();
         for(String key : keys) {
             System.out.printf(key + ": " + idMap.get(key) + "\n");
