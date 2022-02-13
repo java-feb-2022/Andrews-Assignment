80d13718b612a4e75769f48f7320798a939e95ce
DeWittDev <88995967+DeWittDev@users.noreply.github.com>
2022-02-12 22:24:35 -0800

    Copybook 3

--- Week1/Extra Practice/Maps/Map.java
+++ Week1/Extra Practice/Maps/Map.java
@@ -3,17 +3,17 @@ import java.util.Set;
 
 public class Map {
     public static void main(String[] args){
-        HashMap<String, String> idMap = new HashMap<String, String>();
+        HashMap<String, String> trackList = new HashMap<String, String>();
 
-        idMap.put("Gods", "The dog returned to his vomit");
-        idMap.put("Of", "the sow returned to her mire");
-        idMap.put("Copybook", "the burnt fool's bandaged finger went wobbling back to the fire");
-        idMap.put("Headings", "Thus when it happened and the brave new world began, when all men where payed for his suffering, and no man payed for his sins. As surely as water has wet us, as surely as fire has burned. the Gods of the Copybook Headings with terror and slaughter, return.");
+        trackList.put("Gods", "The dog returned to his vomit");
+        trackList.put("Of", "the sow returned to her mire");
+        trackList.put("Copybook", "the burnt fool's bandaged finger went wobbling back to the fire");
+        trackList.put("Headings", "Thus when it happened and the brave new world began, when all men where payed for his suffering, and no man payed for his sins. As surely as water has wet us, as surely as fire has burned. the Gods of the Copybook Headings with terror and slaughter, return.");
 
-        System.out.println(idMap.get("Headings") + "\n\n");
-        Set<String> keys = idMap.keySet();
+        System.out.println(trackList.get("Headings") + "\n\n");
+        Set<String> keys = trackList.keySet();
         for(String key : keys) {
-            System.out.printf(key + ": " + idMap.get(key) + "\n");
+            System.out.printf(key + ": " + trackList.get(key) + "\n");
         }
     }
 }
