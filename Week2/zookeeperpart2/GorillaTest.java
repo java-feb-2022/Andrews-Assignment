72418824b1c3d61a6a305235795fffdbc1c2b4f5
DeWittDev <88995967+DeWittDev@users.noreply.github.com>
2022-04-20 15:14:16 -0700

    Keeper 1

--- Week2/zookeeperpart2/GorillaTest.java
+++ Week2/zookeeperpart2/GorillaTest.java
@@ -3,8 +3,18 @@ package Week2.zookeeperpart2;
 public class GorillaTest {
     public static void main(String[] args) {
         Gorilla g1 = new Gorilla();
+        System.out.println("--------Zoo Keeper Part 1---------");
+        g1.displayEnergy();
+        g1.throwObject();
+        g1.throwObject();
+        g1.throwObject();
         g1.displayEnergy();
         g1.eatBanana();
+        g1.eatBanana();
         g1.displayEnergy();
+        g1.climb();
+        g1.displayEnergy();
+
+        System.out.println("---------Zoo Keeper Part 2---------");
     }
 }
